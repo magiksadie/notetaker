a01475257d08597508721b46b411fe973ea7dc6a
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-21 12:03:09 -0500

    fixed routes, cleaned code, pull routes in server.js

--- routes/apiRoutes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const notesRoutes = require('../apiRoutes/notes');
-
-router.use('/notes', notesRoutes);
-
-module.exports = router;
--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,22 +1,56 @@
 const router = require('express').Router();
-const { notes } = require('../db/db.json');
+const { notes } = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
+
+const newTask = [];
+
+//API Routes
 router.get('/notes', (req, res) => {
-    let results = notes;
-    if (req.query.search) {
-        results = results.filter(note => {
-            return note.title.toLowerCase().includes(req.query.search.toLowerCase()) || note.content.toLowerCase().includes(req.query.search.toLowerCase());
-        });
-    }
-    res.json(results);
+    const returnedNotes = fs.readFileSync(path.join(__dirname, '../../db/db.json'), 'utf8');
+    console.log(returnedNotes);
+    return res.json(JSON.parse(returnedNotes));
 });
 
 router.post('/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = notes.length + 1;
-    notes.push(newNote);
-    res.json(newNote);
-}
-);
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, data) => {
+        if (err) throw res.send(404);
+        const notes = JSON.parse(data);
+        notes.push(req.body);
+        console.log(notes);
+        
+        for (let i=0; i < notes.length; i++) {
+            const addNote = {
+                title: notes[i].title,
+                text: notes[i].text,
+                id: [i],
+            };
+            newTask.push(addNote);
+        }
+
+        fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(newTask, null, 2), (err) => {
+            if (err) throw res.send(404);
+            res.json(req.body);
+        }
+        );
+    });
+});
+
+router.get('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const note = notes.find(note => note.id === parseInt(id));
+    if (!note) return res.status(404).send('Note not found');
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let currentNote = notes;
+    let filteredNote = currentNote.filter(note => note.id != req.params.id);
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(filteredNote, null, 2));
+
+    res.json(filteredNote);
+    notes = filteredNote;
+});
 
 module.exports = router;
